46d71d4d44ee9e12b68ae4f295d9528b59f2ef09
Arun Paul <apaul7445@gmail.com>
2024-12-17 23:01:37 +0530

    tailwind css and radix ui is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@radix-ui/themes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello World
+      <Button>I'm a button</Button>
+      <p className='font-extrabold text-red-600'>Hey it's a tailwind css text paragraph</p>
+      <h1 className="text-3xl text-red-300 font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
